b0793eee0fa900ae4d7adf36171b508f4752b2a7
Jeremy Tan <36715338+jeremymtan@users.noreply.github.com>
2023-11-25 01:01:19 -0500

    sanity check

--- 10_code/chunking_states.py
+++ 10_code/chunking_states.py
@@ -1,10 +1,10 @@
+"""grabs all the states and pareses them into individual parquet files"""
 import pandas as pd
-import concurrent.futures
 
 input_file = "../00_data/arcos_all_washpost.tsv"
 output_dir = "../00_data/states/"
 
-
+"""pre defined states list"""
 STATES = [
     "AK",
     "AL",
@@ -79,20 +79,32 @@ def process_state(state, chunks):
     """saves state as parquet file"""
     state_df = []
     for i, chunk in enumerate(chunks):
+        # converts columns to datetime for later conversion
         chunk["TRANSACTION_DATE"] = pd.to_datetime(
             chunk["TRANSACTION_DATE"], errors="coerce"
         )
-
+        # splits TRANSACTION_DATE to two columns year and month
         chunk["YEAR"] = chunk["TRANSACTION_DATE"].dt.year
         chunk["MONTH"] = chunk["TRANSACTION_DATE"].dt.month
+        # keeps only the columns we discussed
         state_chunk = chunk[columns_to_keep].loc[chunk["BUYER_STATE"] == state]
         if not state_chunk.empty:
             state_df.append(state_chunk)
             print(i)
-            print(state_df)
+            # print(state_df)
+    # grabs all relevant rows related to the searched state
     pd.concat(state_df, ignore_index=True).to_parquet(
         output_dir + state + ".parquet", index=False
     )
+
+
+def grab_states(chunks):
+    """adds to a set of states to check"""
+    states = set()
+    for i, chunk in enumerate(chunks):
+        states.update(chunk["BUYER_STATE"])
+        print(states)
+    return states
 
 
 if __name__ == "__main__":
@@ -100,6 +112,6 @@ if __name__ == "__main__":
 
     for i in STATES:
         print(i)
-        # need to read this again for some reason
+        # need to read this again to remake iterator
         chunks = pd.read_table(path, chunksize=10000000, low_memory=False)
         process_state(i, chunks)
