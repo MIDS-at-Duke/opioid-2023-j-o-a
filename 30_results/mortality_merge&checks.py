b067cce4a3eef779a398ea283c6f533e48d0576d
AaryaDesai1 <aad64@duke.edu>
2023-11-27 18:41:26 -0500

    Adding new code with diff in diff function

--- 30_results/mortality_merge&checks.py
+++ 30_results/mortality_merge&checks.py
@@ -1,24 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import statsmodels.api as sm
 import numpy as np
 
 
-def process_state_consumption(parquet_path):
-    consumption_data = pd.read_parquet(parquet_path)
-    consumption_data.rename(columns={"BUYER_COUNTY": "County"}, inplace=True)
-    consumption_data = consumption_data[consumption_data["County"].notnull()]
-    unique_counties_per_year = consumption_data.groupby("YEAR")["County"].nunique()
-    return consumption_data, unique_counties_per_year
-
-
-def process_mortality_data(csv_path, state_codes):
+def process_mortality_data(csv_path, state_codes=None):
     mortality_data = pd.read_csv(csv_path)
-    if isinstance(state_codes, list):
-        mortality_data = mortality_data[mortality_data["State"].isin(state_codes)]
+    if state_codes is None:
+        mortality_data = mortality_data
     else:
-        mortality_data = mortality_data[mortality_data["State"] == state_codes]
-
+        if isinstance(state_codes, list):
+            mortality_data = mortality_data[mortality_data["State"].isin(state_codes)]
+        else:
+            mortality_data = mortality_data[mortality_data["State"] == state_codes]
     mortality_data["County"] = mortality_data["County"].str.upper()
     mortality_data["County"] = mortality_data["County"].str.replace(" COUNTY", "")
     mortality_data.rename(columns={"Year": "YEAR"}, inplace=True)
@@ -30,17 +25,18 @@ def process_mortality_data(csv_path, state_codes):
     return mortality_data, observations_per_county_year
 
 
-def process_population_data(csv_path, state_codes):
+def process_population_data(csv_path, state_codes=None):
     population_data = pd.read_csv(csv_path)
     population_data.rename(columns={"CTYNAME": "County", "Year": "YEAR"}, inplace=True)
     population_data["County"] = population_data["County"].str.upper()
     population_data["County"] = population_data["County"].str.replace(" COUNTY", "")
-
-    if isinstance(state_codes, list):
-        state_pop_data = population_data[population_data["STATE"].isin(state_codes)]
+    if state_codes is None:
+        state_pop_data = population_data
     else:
-        state_pop_data = population_data[population_data["STATE"] == state_codes]
-
+        if isinstance(state_codes, list):
+            state_pop_data = population_data[population_data["STATE"].isin(state_codes)]
+        else:
+            state_pop_data = population_data[population_data["STATE"] == state_codes]
     # Calculate mean and median population per county
     mean_population = state_pop_data.groupby("County")["Population"].mean()
     median_population = state_pop_data.groupby("County")["Population"].median()
@@ -165,12 +161,26 @@ def impute_for_mortality(pop_mortality_data):
     pop_mortality_data = pop_mortality_data[
         ~pop_mortality_data["County"].isin(dropped_counties)
     ]
+    # pop_mortality_data["Deaths_Revised"] = (
+    #     pop_mortality_data["Death_Rate"] * pop_mortality_data["Population"]
+    # )
     return pop_mortality_data
 
 
+def format_percent(x, pos):
+    return f"{x * 100:.4f}%"
+
+
 def plot_pre_post_ols(df, state_code, policy_year):
-    pre_policy = df[df["YEAR"] < (policy_year - 1)]
-    post_policy = df[df["YEAR"] > (policy_year - 1)]
+    years_before = policy_year - 2003
+    years_after = 2015 - policy_year
+
+    limit = min(
+        years_before, years_after
+    )  # Determine the limit based on the lesser value
+
+    pre_policy = df[(df["YEAR"] >= policy_year - limit) & (df["YEAR"] < policy_year)]
+    post_policy = df[(df["YEAR"] >= policy_year) & (df["YEAR"] <= policy_year + limit)]
 
     X_pre = sm.add_constant(pre_policy["YEAR"])
     y_pre = pre_policy["Death_Rate"]
@@ -206,14 +216,14 @@ def plot_pre_post_ols(df, state_code, policy_year):
     plt.plot(
         post_policy["YEAR"],
         results_post.predict(X_post),
-        color="green",
+        color="orange",
         label=(f"Post-{policy_year} OLS"),
     )
     plt.fill_between(
         post_policy["YEAR"],
         pred_post_ci[:, 0],
         pred_post_ci[:, 1],
-        color="green",
+        color="orange",
         alpha=0.3,
     )
     plt.axvline(
@@ -221,10 +231,136 @@ def plot_pre_post_ols(df, state_code, policy_year):
     )
 
     plt.xlabel("Year")
-    plt.ylabel("Death Rate")
-    plt.title(f"Death Rate in {state_code} (Pre and Post {policy_year})")
+    plt.ylabel("Percentage (%) of Population that died by opioids")
+    plt.title(
+        f"Percentage (%) of Population that died by opioids in Washington (Pre and Post {policy_year})"
+    )
+    formatter = mtick.FuncFormatter(format_percent)
+    plt.gca().yaxis.set_major_formatter(formatter)
     plt.legend()
-    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_diff_in_diff(treatment_df, control_df, policy_year):
+    years_before = policy_year - 2003
+    years_after = 2015 - policy_year
+    limit = min(
+        years_before, years_after
+    )  # Determine the limit based on the lesser value
+    treatment_pre_policy = treatment_df[
+        (treatment_df["YEAR"] >= policy_year - limit)
+        & (treatment_df["YEAR"] < policy_year)
+    ]
+    treatment_post_policy = treatment_df[
+        (treatment_df["YEAR"] >= policy_year)
+        & (treatment_df["YEAR"] <= policy_year + limit)
+    ]
+    control_pre_policy = control_df[
+        (control_df["YEAR"] >= policy_year - limit) & (control_df["YEAR"] < policy_year)
+    ]
+    control_post_policy = control_df[
+        (control_df["YEAR"] >= policy_year)
+        & (control_df["YEAR"] <= policy_year + limit)
+    ]
+
+    X_treatment_pre = sm.add_constant(treatment_pre_policy["YEAR"])
+    y_treatment_pre = treatment_pre_policy["Death_Rate"]
+    model_treatment_pre = sm.OLS(y_treatment_pre, X_treatment_pre)
+    results_treatment_pre = model_treatment_pre.fit()
+
+    X_treatment_post = sm.add_constant(treatment_post_policy["YEAR"])
+    y_treatment_post = treatment_post_policy["Death_Rate"]
+    model_treatment_post = sm.OLS(y_treatment_post, X_treatment_post)
+    results_treatment_post = model_treatment_post.fit()
+
+    X_control_pre = sm.add_constant(control_pre_policy["YEAR"])
+    y_control_pre = control_pre_policy["Death_Rate"]
+    model_control_pre = sm.OLS(y_control_pre, X_control_pre)
+    results_control_pre = model_control_pre.fit()
+
+    X_control_post = sm.add_constant(control_post_policy["YEAR"])
+    y_control_post = control_post_policy["Death_Rate"]
+    model_control_post = sm.OLS(y_control_post, X_control_post)
+    results_control_post = model_control_post.fit()
+
+    plt.figure(figsize=(8, 6))
+    # Confidence interval bands for Pre-2010
+    pred_treatment_pre = results_treatment_pre.get_prediction(X_treatment_pre)
+    pred_treatment_pre_ci = pred_treatment_pre.conf_int()
+    plt.plot(
+        treatment_pre_policy["YEAR"],
+        results_treatment_pre.predict(X_treatment_pre),
+        color="blue",
+        label=(f"Treatment State: FL"),
+    )
+    plt.fill_between(
+        treatment_pre_policy["YEAR"],
+        pred_treatment_pre_ci[:, 0],
+        pred_treatment_pre_ci[:, 1],
+        color="blue",
+        alpha=0.3,
+    )
+
+    pred_control_pre = results_control_pre.get_prediction(X_control_pre)
+    pred_control_pre_ci = pred_control_pre.conf_int()
+    plt.plot(
+        control_pre_policy["YEAR"],
+        results_control_pre.predict(X_control_pre),
+        color="orange",
+        label=(f"Control States: KY, WV, TN, NV, OR"),
+    )
+    plt.fill_between(
+        control_pre_policy["YEAR"],
+        pred_control_pre_ci[:, 0],
+        pred_control_pre_ci[:, 1],
+        color="orange",
+        alpha=0.3,
+    )
+
+    # Confidence interval bands for Post-2010
+    pred_treatment_post = results_treatment_post.get_prediction(X_treatment_post)
+    pred_treatment_post_ci = pred_treatment_post.conf_int()
+    plt.plot(
+        treatment_post_policy["YEAR"],
+        results_treatment_post.predict(X_treatment_post),
+        color="blue",
+    )
+    plt.fill_between(
+        treatment_post_policy["YEAR"],
+        pred_treatment_post_ci[:, 0],
+        pred_treatment_post_ci[:, 1],
+        color="blue",
+        alpha=0.3,
+    )
+
+    pred_control_post = results_control_post.get_prediction(X_control_post)
+    pred_control_post_ci = pred_control_post.conf_int()
+    plt.plot(
+        control_post_policy["YEAR"],
+        results_control_post.predict(X_control_post),
+        color="orange",
+    )
+    plt.fill_between(
+        control_post_policy["YEAR"],
+        pred_control_post_ci[:, 0],
+        pred_control_post_ci[:, 1],
+        color="orange",
+        alpha=0.3,
+    )
+    plt.axvline(
+        x=policy_year, color="black", linestyle="--", label=(f"Year {policy_year}")
+    )
+
+    plt.xlabel("Year")
+    plt.ylabel("Percentage (%) of Population that died by opioids")
+    plt.title(
+        f"Percentage (%) of Population that died by opioids (Pre and Post {policy_year})"
+    )
+    formatter = mtick.FuncFormatter(format_percent)
+    plt.gca().yaxis.set_major_formatter(formatter)
+    plt.legend()
+    plt.tight_layout()
     plt.show()
 
 
@@ -234,14 +370,73 @@ wa_pop = process_population_data("../00_data/PopFips.csv", "FL")
 clean_county(wa_mort)
 clean_county(wa_pop)
 wa_pop_mort = merge_mortality_population(wa_pop, wa_mort, 2003, 2015)
-print(check_unique_counties_per_year(wa_pop_mort, wa_pop))
-some_null_counties, all_null_counties, no_null_counties = get_death_null_counties(
-    wa_pop_mort
-)
-find_mismatched_counties(wa_pop_mort)
+# print(check_unique_counties_per_year(wa_pop_mort, wa_pop))
+# some_null_counties, all_null_counties, no_null_counties = get_death_null_counties(
+#     wa_pop_mort
+# )
+# find_mismatched_counties(wa_pop_mort)
 imputed_wa_pop_mort = impute_for_mortality(wa_pop_mort)
 
-print(wa_pop_mort["Deaths"].isnull().sum())
-print(imputed_wa_pop_mort["Death_Rate"].isnull().sum())
+# print(wa_pop_mort["Deaths"].isnull().sum())
+# print(imputed_wa_pop_mort["Death_Rate"].isnull().sum())
 
-plot_pre_post_ols(imputed_wa_pop_mort, "WA", 2010)
+wa_controls_mort, o = process_mortality_data(
+    "../00_data/mortality_final.csv", ["KY", "WV", "TN", "NV", "OR"]
+)
+wa_controls_pop = process_population_data(
+    "../00_data/PopFips.csv", ["KY", "WV", "TN", "NV", "OR"]
+)
+clean_county(wa_controls_mort)
+clean_county(wa_controls_pop)
+wa_controls_pop_mort = merge_mortality_population(
+    wa_controls_pop, wa_controls_mort, 2003, 2015
+)
+imputed_wa_controls = impute_for_mortality(wa_controls_pop_mort)
+print(wa_controls_pop_mort["Deaths"].isnull().sum())
+print(imputed_wa_controls["Death_Rate"].isnull().sum())
+
+plot_diff_in_diff(imputed_wa_pop_mort, imputed_wa_controls, 2010)
+
+
+def calculate_slope(df):
+    X = sm.add_constant(df["YEAR"])
+    y = df["Death_Rate"]
+    model = sm.OLS(y, X)
+    results = model.fit()
+    return results.params[1]  # Return the slope coefficient
+
+
+# Assuming your mortality dataset is stored in mortality_data variable
+# 'treatment_state_code' is the code of your treatment state
+
+# mort, o = process_mortality_data("../00_data/mortality_final.csv")
+# pop = process_population_data("../00_data/PopFips.csv")
+# clean_county(mort)
+# clean_county(pop)
+# mortality_data_1 = merge_mortality_population(pop, mort, 2003, 2015)
+# mortality_data = impute_for_mortality(mortality_data_1)
+# treatment_state_data = mortality_data[mortality_data["STATE"] == "WA"]
+
+# # Calculate slope for treatment state
+# slope_treatment_state = calculate_slope(treatment_state_data)
+
+# similar_states = []
+# for state_code, state_df in mortality_data.groupby("STATE"):
+#     if state_code != "WA":  # Skip the treatment state
+#         pre_policy_data = state_df[state_df["YEAR"] < 2012]
+#         if len(pre_policy_data) > 1:  # At least two years needed for slope calculation
+#             slope = calculate_slope(pre_policy_data)
+#             similar_states.append((state_code, slope))
+
+# # Sort the states based on the absolute difference in slope
+# similar_states.sort(key=lambda x: abs(x[1] - slope_treatment_state))
+
+# # Select the states with slopes most similar to the treatment state
+# num_states_to_select = 10  # Change this number as needed
+# selected_states = similar_states[:num_states_to_select]
+
+# print("States with slopes most similar to WA:")
+# for state_code, slope in selected_states:
+#     print(
+#         f"State Code: {state_code}, Slope Difference: {slope - slope_treatment_state}"
+#     )
