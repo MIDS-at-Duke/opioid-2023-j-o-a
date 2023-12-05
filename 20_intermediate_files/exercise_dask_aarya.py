47e442377d43c9da7e3d89efb726ea3ce6fe9edb
AaryaDesai1 <aad64@duke.edu>
2023-12-05 10:50:19 -0500

    Adding final PDS exercise - Aarya

--- 20_intermediate_files/exercise_dask_aarya.py
+++ 20_intermediate_files/exercise_dask_aarya.py
@@ -1,5 +1,6 @@
 import dask.dataframe as dd
 
+# Specify the columns that are required
 required_cols = [
     "BUYER_STATE",
     "BUYER_ZIP",
@@ -10,12 +11,14 @@ required_cols = [
     "MME",
 ]
 
+# Read the data into Dask DataFrame with the required columns
 consumption_df_dask = dd.read_csv(
     "/Users/aaryadesai/Downloads/arcos_all_washpost.tsv",
     sep="\t",
     usecols=required_cols,
 )
 
+# Add the year and month columns from the TRANSACTION_DATE column using the .dt() function
 consumption_df_dask["Year"] = dd.to_datetime(
     consumption_df_dask["TRANSACTION_DATE"], format="%Y-%m-%d"
 ).dt.year
@@ -23,13 +26,15 @@ consumption_df_dask["Month"] = dd.to_datetime(
     consumption_df_dask["TRANSACTION_DATE"], format="%Y-%m-%d"
 ).dt.month
 
+# Group the data by the required columns and sum the MME column for consumption calculations
 consumption_df_dask = (
     consumption_df_dask.groupby(["BUYER_COUNTY", "BUYER_STATE", "Year"])["MME"]
     .sum()
     .reset_index()
 )
 
+# Trigger the computation by calling the .compute() method
 final_df_dask = consumption_df_dask.compute()
 
-print("The head of the df is: ")
+# To check if the df is correct and the code is working
 final_df_dask.head()
